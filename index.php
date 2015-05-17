8790da5a9336446c96f9f52ba0060a5444ab3a12
Teije van Sloten <teije.van.sloten@hva.nl>
2015-05-17 18:52:38 +0200

    More styling for a basic page

--- index.php
+++ index.php
@@ -15,15 +15,19 @@
         <![endif]-->
     </head>
     <body>
-        <div style="background: black;">
+        <div class="title-heading">
             <div class="container">
                 <div class="header clearfix">
-                    <h1 class="logo">Memory Light</h1>
-                    <p class="text-capitalize logo-statement">Professionals in support bereavement care</p>
+                    <h1 class="logo">
+                        Memory Light
+                    </h1>
+                    <p class="text-capitalize logo-statement">
+                        Professionals in support bereavement care
+                    </p>
                 </div>
             </div>
         </div>
-        <div style="background: #222;">
+        <div class="menu-heading">
             <div class="container">
                 <nav>
                     <ul class="nav nav-tabs pull-right">
@@ -36,13 +40,60 @@
             </div>
         </div>
         <div class="container">
-            <h1>Memory Light</h1>
-            <p class="lead">Memory Light content.</p>
-            <p>Losing a loved one is incredibly difficult in every stage of one's life.
-                Sometimes words aren't enough to help a loved one through their grief.
-                That's why Memory Light VOF Netherlands developed unique products to support bereavement care.
-            </p>
+
+            <div class="row">
+                <div class="col-md-8" style="padding-top:75px;">
+                    <p class="page-paragraph">
+                        Losing a loved one is incredibly difficult in every stage of one's life.
+                        Sometimes words aren't enough to help a loved one through their grief.
+                        That's why Memory Light VOF Netherlands developed unique products to support bereavement care.
+                    </p>
+                </div>
+                <div class="col-md-4" style="padding-top: 50px;">
+                    <img src="img/candle-mate-temp.jpg" alt="temp img" class="pull-right">
+                </div>
+            </div>
+
+            <div class="row"><hr></div>
+
+            <div class="row">
+                <div class="col-md-8" style="padding-top:75px;">
+                    <p class="page-paragraph">
+                        Losing a loved one is incredibly difficult in every stage of one's life.
+                        Sometimes words aren't enough to help a loved one through their grief.
+                        That's why Memory Light VOF Netherlands developed unique products to support bereavement care.
+                    </p>
+                </div>
+                <div class="col-md-4" style="padding-top: 50px;">
+                    <img src="img/candle-mate-temp.jpg" class="pull-right">
+                </div>
+            </div>
         </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="col-md-3 col-md-offset-1">
+                    <h5>Memory Light</h5>
+                    <ul class="list-unstyled small">
+                        <li>Memory Light VOF</li>
+                        <li>P.O Box</li>
+                        <li>Delft</li>
+                        <li>Mobile</li>
+                        <li><a href="mailto:info@memorylight.eu">info@memorylight.eu</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-md-offset-3">
+                    <h5>Memory Light</h5>
+                    <ul class="list-unstyled small">
+                        <li>Memory Light VOF</li>
+                        <li>P.O Box</li>
+                        <li>Delft</li>
+                        <li>Mobile</li>
+                        <li><a href="mailto:info@memorylight.eu">info@memorylight.eu</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
     </body>
