39610c3a0650afcb66c4f93e9c91effeaf6c0a74
Teije van Sloten <teije.van.sloten@hva.nl>
2015-05-31 15:32:33 +0200

    1 option added.

---
+++ index_1.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom_1.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="title-heading">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="header clearfix">
+                            <h1 class="logo">
+                                Memory Light
+                            </h1>
+                            <p class="text-capitalize logo-statement">
+                                Professionals in support bereavement care
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-8" style="margin-top: 54px;">
+                            <ul class="nav nav-tabs pull-right">
+                                <li role="presentation" class="active"><a href="#">Home</a></li>
+                                <li role="presentation"><a href="#">Memory Light Relic</a></li>
+                                <li role="presentation"><a href="#">Memory Candle Dispenser</a></li>
+                                <li role="presentation"><a href="#">Partner Program</a></li>
+                            </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="col-md-3 col-md-offset-1">
+                    <h5>Memory Light</h5>
+                    <ul class="list-unstyled small">
+                        <li>Memory Light VOF</li>
+                        <li>P.O Box</li>
+                        <li>Delft</li>
+                        <li>Mobile</li>
+                        <li><a href="mailto:info@memorylight.eu">info@memorylight.eu</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-md-offset-3">
+                    <h5>Memory Light</h5>
+                    <ul class="list-unstyled small">
+                        <li>Memory Light VOF</li>
+                        <li>P.O Box</li>
+                        <li>Delft</li>
+                        <li>Mobile</li>
+                        <li><a href="mailto:info@memorylight.eu">info@memorylight.eu</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
