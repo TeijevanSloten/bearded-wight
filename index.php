efc6d4eba59142adfbe3e858f9e812feff63cd88
Teije van Sloten <teije.van.sloten@hva.nl>
2015-06-14 17:08:30 +0200

    Favicon added.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="css/theme.css" rel="stylesheet">
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
 
         <link rel="stylesheet" href="css/vendor/owl-carousel/owl.carousel.css">
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
