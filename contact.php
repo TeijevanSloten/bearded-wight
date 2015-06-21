a82b52293b2fb8dac99c38ab82c2b6019bfd2374
Teije van Sloten <teije.van.sloten@hva.nl>
2015-06-21 16:42:21 +0200

    Footer extracted.

--- contact.php
+++ contact.php
@@ -133,46 +133,15 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                                <button type="submit" class="btn btn-lg col-md-3 pull-right btn-warning">Submit</button>
+                            <div class="col-sm-offset-9 col-sm-3">
+                                <button type="submit" class="btn btn-lg pull-right btn-warning">Submit</button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-1">
-                        <h4 class="footer-title">About</h4>
-                        <p>
-                            Losing a loved one is incredibly difficult.
-                            Sometimes words aren't enough to help.<br>
-                            That's why Memory Light Netherlands developed unique products to support bereavement care.
-                        </p>
-                    </div>
-                    <div class="col-md-3 col-md-offset-1">
-                        <h4 class="footer-title ">Get in touch!</h4>
-                        <a href="mailto:info@memorylight.eu" class="footer-link">
-                            <div class="footer-icon"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></div>
-                        </a>
-                        <a href="#" class="footer-link">
-                            <div class="footer-icon"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span></div>
-                        </a>
-                        <a href="contact.php" class="footer-link">
-                            <div class="footer-icon"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span></div>
-                        </a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <p class="text-center"><a href="privacy.php" class="footer-link">privacy policy</a> - made by mdtvs</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include 'components/footer.php'; ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/vendor/bootstrap/bootstrap.min.js"></script>
         <script src="js/submitForm.js"></script>
--- relic.php
+++ relic.php
@@ -105,37 +105,7 @@
             </div>
         </div>
         
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-1">
-                        <h4 class="footer-title">About</h4>
-                        <p>
-                            Losing a loved one is incredibly difficult.
-                            Sometimes words aren't enough to help.<br>
-                            That's why Memory Light Netherlands developed unique products to support bereavement care.
-                        </p>
-                    </div>
-                    <div class="col-md-3 col-md-offset-1">
-                        <h4 class="footer-title ">Get in touch!</h4>
-                        <a href="mailto:info@memorylight.eu" class="footer-link">
-                            <div class="footer-icon"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></div>
-                        </a>
-                        <a href="#" class="footer-link">
-                            <div class="footer-icon"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span></div>
-                        </a>
-                        <a href="contact.php" class="footer-link">
-                            <div class="footer-icon"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span></div>
-                        </a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <p class="text-center"><a href="privacy.php" class="footer-link">privacy policy</a> - made by mdtvs</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include 'components/footer.php'; ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="js/vendor/bootstrap/bootstrap.min.js"></script>
     </body>
